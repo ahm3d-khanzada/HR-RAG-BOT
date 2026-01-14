874a787145d718e2b6b638cf6cf5ef56817938c8
ahm3d-khanzada <ahm3d.workspace@gmail.com>
2026-01-14 16:57:59 +0500

    Add docker in it

--- SERVER/config/db.py
+++ SERVER/config/db.py
@@ -5,9 +5,13 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME = os.getenv("DB_NAME")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017")
+DB_NAME = os.getenv("DB_NAME", "hr_chatbot")
 
-client = AsyncIOMotorClient(MONGO_URI)
+client = AsyncIOMotorClient(
+    MONGO_URI,
+    serverSelectionTimeoutMS=5000
+)
+
 db = client[DB_NAME]
 users_collection = db["users"]
